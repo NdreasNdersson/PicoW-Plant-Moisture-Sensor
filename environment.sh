a042958f5c9127a141821dc534795e8bf35862f0
Andreas Andersson <e.andreas.asson@gmail.com>
2024-02-07 20:42:06 +0100

    Add pre-commit

--- environment.sh
+++ environment.sh
@@ -6,3 +6,7 @@ export FREERTOS_KERNEL_PATH=${SCRIPT_DIR}/external/FreeRTOS-Kernel
 export PICO_SDK_PATH=${SCRIPT_DIR}/external/pico-sdk
 export JSON_MAKER_PATH=${SCRIPT_DIR}/external/json-maker
 export TINY_JSON_PATH=${SCRIPT_DIR}/external/tiny-json
+
+pip install -r requirements.txt
+
+pre-commit install
