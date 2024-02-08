d6b6e9808c9a12f95a13e1f6315b4f2963c3b6a5
Andreas Andersson <e.andreas.asson@gmail.com>
2024-02-08 10:16:07 +0100

    Update example_device.cmake

--- creds/example_device.cmake
+++ creds/example_device.cmake
@@ -14,30 +14,3 @@ if(NOT WIFI_PASSWORD_SET)
     set(WIFI_PASSWORD_SET "MyPassword") # Put your own WiFi Password
     message("WiFi PASSWORD set: *******")
 endif()
-
-# Configure MQTT credentials
-message("Configuring MQTT Credentials.....")
-if(NOT MQTT_CLIENT_SET)
-    set(MQTT_CLIENT_SET "device1") # Put your own MQTT Client ID
-    message("MQTT Client ID set: ${MQTT_CLIENT_SET}")
-endif()
-
-if(NOT MQTT_USER_SET)
-    set(MQTT_USER_SET "rackadev") # Put your own MQTT USER
-    message("MQTT USER set: *******")
-endif()
-
-if(NOT MQTT_PASSWD_SET)
-    set(MQTT_PASSWD_SET "MyPassword") # Put your own MQTT password
-    message("MQTT Password set: *******")
-endif()
-
-if(NOT MQTT_HOST_SET)
-    set(MQTT_HOST_SET "www.mqtt.com") # Put your own MQTT HOST
-    message("MQTT HOST set: *******")
-endif()
-
-if(NOT MQTT_PORT_SET)
-    set(MQTT_PORT_SET 9090) # Put your own MQTT PORT
-    message("MQTT PORT set: ${MQTT_PORT_SET}")
-endif()
