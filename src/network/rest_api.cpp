b3a31a406c45b1e38a7ecb76b50518a7960a1343
Andreas Andersson <e.andreas.asson@gmail.com>
2024-03-18 20:54:43 +0100

    23 implement set and get restapi (#42)
    * Get Post identified
    
    * Post req saved to buffer
    
    * Set config in flash from RestAPI
    
    * Remove commented row

--- src/network/rest_api.cpp
+++ src/network/rest_api.cpp
@@ -1,12 +1,20 @@
 #include "rest_api.h"
 
 #include <algorithm>
+#include <cstring>
 
 #include "FreeRTOS.h"
 #include "nlohmann/json.hpp"
 #include "semphr.h"
 #include "utils/logging.h"
 using json = nlohmann::json;
+
+static const std::string HTTP_OK_RESPONSE{"HTTP/1.0 200 OK\r\n"};
+
+static const std::string HTTP_BAD_RESPONSE{"HTTP/1.0 400 NOK\r\n"};
+
+static const std::string HTTP_CONTENT_TYPE{
+    "Content-type: application/json\r\n\r\n"};
 
 RestApi::RestApi()
     : m_ip_address{"0.0.0.0"},
@@ -22,19 +30,6 @@ bool RestApi::start() {
     if (m_server_state == NULL) {
         return false;
     }
-
-    std::string header_str{
-        "HTTP/1.0 200 OK\r\nContent-type: application/json\r\n\r\n"};
-    std::string body_str{"{}"};
-    for (int i = 0; i < header_str.size(); i++) {
-        m_server_state->buffer_sent[0][i] = header_str[i];
-    }
-    for (int i = 0; i < body_str.size(); i++) {
-        m_server_state->buffer_sent[1][i] = body_str[i];
-    }
-    m_server_state->buffer_send_len[0] = header_str.size();
-    m_server_state->buffer_send_len[1] = body_str.size();
-    m_server_state->packages_send_len = 2;
 
     LogInfo(("Starting server at %s on port %u\n",
              ip4addr_ntoa(netif_ip4_addr(netif_list)), m_port));
@@ -93,9 +88,24 @@ bool RestApi::set_data(const std::string &device_name,
     return true;
 }
 
-void RestApi::update() {
-    /* char *dest = reinterpret_cast<char *>(m_server_state->buffer_sent[1]); */
+bool RestApi::get_data(std::string &data) {
+    auto status{false};
+    if (xSemaphoreTake(m_server_state->buffer_mutex,
+                       static_cast<TickType_t>(100)) == pdTRUE) {
+        if (m_server_state->data_received) {
+            data = std::string(
+                reinterpret_cast<char const *>(m_server_state->buffer_recv),
+                m_server_state->buffer_recv_len);
+            status = true;
+            m_server_state->data_received = false;
+        }
+        xSemaphoreGive(m_server_state->buffer_mutex);
+    }
 
+    return status;
+}
+
+void RestApi::update() {
     json json_data;
     for (auto &device : m_devices) {
         if (device.is_registered()) {
@@ -107,9 +117,166 @@ void RestApi::update() {
                        static_cast<TickType_t>(100)) == pdTRUE) {
         const char *json_data_str{json_data.dump().c_str()};
 
-        memcpy(m_server_state->buffer_sent[1], json_data_str,
-               strlen(json_data_str));
-        m_server_state->buffer_send_len[1] = strlen(json_data_str);
+        std::memcpy(m_server_state->device_data, json_data_str,
+                    strlen(json_data_str));
+        m_server_state->device_data_len = strlen(json_data_str);
         xSemaphoreGive(m_server_state->buffer_mutex);
     }
+}
+
+err_t RestApi::tcp_client_close(void *arg) {
+    TCP_SERVER_T *state = (TCP_SERVER_T *)arg;
+    err_t err = ERR_OK;
+    if (state->client_pcb != NULL) {
+        tcp_arg(state->client_pcb, NULL);
+        tcp_poll(state->client_pcb, NULL, 0);
+        tcp_sent(state->client_pcb, NULL);
+        tcp_recv(state->client_pcb, NULL);
+        tcp_err(state->client_pcb, NULL);
+        err = tcp_close(state->client_pcb);
+        if (err != ERR_OK) {
+            LogError(("close failed %d, calling abort", err));
+            tcp_abort(state->client_pcb);
+            err = ERR_ABRT;
+        }
+        state->client_pcb = NULL;
+    }
+    return err;
+}
+
+err_t RestApi::tcp_server_close(void *arg) {
+    TCP_SERVER_T *state = (TCP_SERVER_T *)arg;
+    err_t err = ERR_OK;
+    if (state->client_pcb != NULL) {
+        tcp_arg(state->client_pcb, NULL);
+        tcp_poll(state->client_pcb, NULL, 0);
+        tcp_sent(state->client_pcb, NULL);
+        tcp_recv(state->client_pcb, NULL);
+        tcp_err(state->client_pcb, NULL);
+        err = tcp_close(state->client_pcb);
+        if (err != ERR_OK) {
+            LogError(("close failed %d, calling abort", err));
+            tcp_abort(state->client_pcb);
+            err = ERR_ABRT;
+        }
+        state->client_pcb = NULL;
+    }
+    if (state->server_pcb) {
+        tcp_arg(state->server_pcb, NULL);
+        tcp_close(state->server_pcb);
+        state->server_pcb = NULL;
+    }
+    return err;
+}
+
+err_t RestApi::tcp_server_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) {
+    TCP_SERVER_T *state = (TCP_SERVER_T *)arg;
+    LogDebug(("tcp_server_sent %u", len));
+
+    return ERR_OK;
+}
+
+err_t RestApi::tcp_server_send(void *arg, struct tcp_pcb *tpcb,
+                               std::string data) {
+    cyw43_arch_lwip_check();
+    err_t err = tcp_write(tpcb, data.c_str(), strlen(data.c_str()),
+                          TCP_WRITE_FLAG_COPY);
+    if (err != ERR_OK) {
+        LogError(("Failed to write data %d", err));
+        return tcp_client_close(arg);
+    }
+    return ERR_OK;
+}
+
+err_t RestApi::tcp_server_send_measured_data(void *arg, struct tcp_pcb *tpcb) {
+    TCP_SERVER_T *state = (TCP_SERVER_T *)arg;
+
+    cyw43_arch_lwip_check();
+    if (xSemaphoreTake(state->buffer_mutex, (TickType_t)100) == pdTRUE) {
+        err_t err = tcp_write(tpcb, state->device_data, state->device_data_len,
+                              TCP_WRITE_FLAG_COPY);
+        xSemaphoreGive(state->buffer_mutex);
+        if (err != ERR_OK) {
+            LogError(("Failed to write data %d", err));
+            return tcp_client_close(arg);
+        }
+    }
+    return ERR_OK;
+}
+
+err_t RestApi::tcp_server_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p,
+                               err_t err) {
+    TCP_SERVER_T *state = (TCP_SERVER_T *)arg;
+    if (!p) {
+        LogDebug(("tcp_server_recv pointer empty"));
+        return tcp_client_close(arg);
+    }
+    cyw43_arch_lwip_check();
+    if (p->tot_len > 0) {
+        std::string payload(reinterpret_cast<char const *>(p->payload), p->len);
+
+        if (payload.rfind("GET", 0) == 0) {
+            LogDebug(("Received GET"));
+
+            if (tcp_server_send(arg, tpcb,
+                                HTTP_OK_RESPONSE + HTTP_CONTENT_TYPE) ==
+                ERR_OK) {
+                tcp_server_send_measured_data(arg, state->client_pcb);
+            }
+        } else if (payload.rfind("POST", 0) == 0) {
+            LogDebug(("Received POST"));
+
+            std::size_t start_idx = payload.find("{");
+            if (start_idx != std::string::npos) {
+                auto json_data{payload.substr(start_idx)};
+                LogDebug(("Received Json: %s", json_data.c_str()));
+                if (xSemaphoreTake(state->buffer_mutex, (TickType_t)100) ==
+                    pdTRUE) {
+                    std::memset(state->buffer_recv, 0, MAX_BUF_SIZE);
+                    std::memcpy(state->buffer_recv, json_data.c_str(),
+                                strlen(json_data.c_str()));
+                    state->buffer_recv_len = strlen(json_data.c_str());
+                    state->data_received = true;
+                    xSemaphoreGive(state->buffer_mutex);
+                }
+                tcp_server_send(arg, tpcb, HTTP_OK_RESPONSE);
+            } else {
+                tcp_server_send(arg, tpcb, HTTP_BAD_RESPONSE);
+            }
+        } else {
+            LogDebug(("Command not implemented. Received:\n", payload.c_str()));
+            tcp_server_send(arg, tpcb, HTTP_BAD_RESPONSE);
+        }
+        tcp_recved(tpcb, p->tot_len);
+    }
+    pbuf_free(p);
+
+    return tcp_client_close(arg);
+}
+
+void RestApi::tcp_server_err(void *arg, err_t err) {
+    if (err != ERR_ABRT) {
+        LogError(("tcp_client_err_fn %d", err));
+        tcp_server_close(arg);
+    }
+}
+
+err_t RestApi::tcp_server_accept(void *arg, struct tcp_pcb *client_pcb,
+                                 err_t err) {
+    TCP_SERVER_T *state = (TCP_SERVER_T *)arg;
+    if (err != ERR_OK || client_pcb == NULL) {
+        LogError(("Failure in accept"));
+        tcp_client_close(arg);
+        return ERR_VAL;
+    }
+
+    LogDebug(("Client connected"));
+
+    state->client_pcb = client_pcb;
+    tcp_arg(client_pcb, state);
+    tcp_sent(client_pcb, tcp_server_sent);
+    tcp_recv(client_pcb, tcp_server_recv);
+    tcp_err(client_pcb, tcp_server_err);
+
+    return ERR_OK;
 }
--- src/network/rest_api.h
+++ src/network/rest_api.h
@@ -5,10 +5,18 @@
 #include <memory>
 #include <string>
 
+#include "FreeRTOS.h"
 #include "device_info.h"
-#include "tcp_server.h"
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+#include "semphr.h"
 
 #define N_DEVICES (4 * 4)
+#define MAX_BUF_SIZE 1024
+#define MAX_PACKAGES 2
+#define POLL_TIME_S 5
 
 class RestApi {
    public:
@@ -19,14 +27,37 @@ class RestApi {
     bool register_device(const std::string &device_name,
                          const std::string &init_value);
     bool set_data(const std::string &device_name, const std::string &new_value);
+    bool get_data(std::string &data);
 
    private:
+    typedef struct TCP_SERVER_T_ {
+        struct tcp_pcb *server_pcb;
+        struct tcp_pcb *client_pcb;
+        uint8_t device_data[MAX_BUF_SIZE];
+        uint8_t buffer_recv[MAX_BUF_SIZE];
+        bool data_received;
+        int device_data_len;
+        int buffer_recv_len;
+        SemaphoreHandle_t buffer_mutex;
+    } TCP_SERVER_T;
+
     std::string m_ip_address;
     int m_port;
-    std::unique_ptr<TCP_SERVER_T> m_server_state;
     std::array<DeviceInfo, N_DEVICES> m_devices;
+    std::unique_ptr<TCP_SERVER_T> m_server_state;
 
     void update();
+    static err_t tcp_client_close(void *arg);
+    static err_t tcp_server_close(void *arg);
+    static err_t tcp_server_sent(void *arg, struct tcp_pcb *tpcb, u16_t len);
+    static err_t tcp_server_send(void *arg, struct tcp_pcb *tpcb,
+                                 std::string data);
+    static err_t tcp_server_send_measured_data(void *arg, struct tcp_pcb *tpcb);
+    static err_t tcp_server_recv(void *arg, struct tcp_pcb *tpcb,
+                                 struct pbuf *p, err_t err);
+    static void tcp_server_err(void *arg, err_t err);
+    static err_t tcp_server_accept(void *arg, struct tcp_pcb *client_pcb,
+                                   err_t err);
 };
 
 #endif
