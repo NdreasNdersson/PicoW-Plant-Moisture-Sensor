20c76cad75e38e7e2cdffe2df12d3d90f451097f
Andreas Andersson <e.andreas.asson@gmail.com>
2024-09-10 14:27:28 +0200

    Run static code analysis only on project files

--- run_codechecker_and_create_report.sh
+++ run_codechecker_and_create_report.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -eux
 
-CodeChecker analyze -o results build/compile_commands.json --enable sensitive -i skip.list
+CodeChecker analyze -o results build/compile_commands.json --config .codechecker.json
 
 CodeChecker parse --export html --output ./reports_html ./results
